d5e62cf3cbe79ceaacf2c4cc6853c44c5a34ade3
daominhtam2k6 <127100950+daominhtam2k6@users.noreply.github.com>
2024-03-21 21:35:18 +0700

    Create events.py

---
+++ events.py
@@ -0,0 +1,24 @@
+# Import thư viện
+
+import discord
+
+# Sự kiện
+@client.event
+
+# Tạo phản hồi đơn giản
+async def respond(message):
+  if message.author == client.user:
+    return
+
+contents = message.content.lower()
+
+response = {
+  "hi":"f{message.author.mention} konnichiwa~!
+
+  # Thêm các phản hồi
+
+}
+
+reply = response.get(content)
+if reply:
+  await message.channel.send(reply)
