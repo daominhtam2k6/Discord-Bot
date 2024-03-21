b5e9200b50034777cd4cf0d16583a8dc6802755d
daominhtam2k6 <127100950+daominhtam2k6@users.noreply.github.com>
2024-03-21 21:04:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Import các thư viện cần thiết
+
+import discord
+
+# Khởi tạo bot/client với các intenst
+
+intents = discord.Intents.all()  # Cấp cho bot toàn bộ quyền 
+client = discord.Client(intents = intents)  
+
+# Truy cập token bot
+TOKEN = os.environ.get('TOKEN')
+# >> Tạo một biến môi trường lưu token của bot để đảm bảo về bảo mật <<
+# Có thể dùng token trực tiếp: TOKEN = 'YOUR_BOT_TOKEN'
+
+# Chạy bot
+client.run(TOKEN)
