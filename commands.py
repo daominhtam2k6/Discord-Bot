4967dd74385423efb9315f60197c5dfee8b1f30f
daominhtam2k6 <127100950+daominhtam2k6@users.noreply.github.com>
2024-03-24 11:09:46 +0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,1 +1,19 @@
-# Buồn ngủ quá~! Mai làm...
+# Import thư viện discord.ext
+
+import discord
+from discord.ext import commands
+
+# Tạo một lệnh đơn giản
+@commands.slash_command(name = "kiss", decription = "emuach")
+async def kiss(ctx, member: discord.Member = None):
+  if not member:
+    member = ctx.author
+    await ctx.reply("Hu?")
+    return
+    
+    kissing = f"{ctx.author.mention} kisses {member.mention}"
+    link = "https://i.gifer.com/XJis.gif"
+    embed = discord.Embed()
+    embed.set_image(url = link)
+    await ctx.reply(kissing, embed = embed)
+    
